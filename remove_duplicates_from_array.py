a10de4cfef9bb19c539d009f31741b25ad299e81
snehac2407 <choudhary2406sneha@gmail.com>
2024-05-28 07:38:22 +0530

    i am learing

---
+++ remove_duplicates_from_array.py
@@ -0,0 +1,16 @@
+def remove_duplicates(sorted_array):
+    if not sorted_array:
+        return []
+
+    ptr = [sorted_array[0]]
+
+    for i in range(1, len(sorted_array)):
+        if sorted_array[i] != sorted_array[i-1]:
+            ptr.append(sorted_array[i])
+
+    return ptr
+
+# Example usage:
+input_array = [1, 1, 2, 2, 3, 3, 3]
+output_array = remove_duplicates(input_array)
+print(output_array)  # Output: [1, 2, 3]
